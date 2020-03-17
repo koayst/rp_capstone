f4afa336abdcb55e4fc835eb9064024a7078b253
koayst <koayst@gmail.com>
2020-03-17 14:00:17 +0800

    ST: update on 17 Mar 2020, 2.00pm

---
+++ extractiveServer_flask.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/url', methods=['GET', 'POST'])
+def url_request():
+    url_req = request.json
+    print()
+    print(url_req['search_url'])
+    print()
+    return jsonify(
+        filename='data\\cna_news_for_testing.csv'
+    )
+    return response
+
+@app.route('/')
+def index():
+    return('<h1>INDEX</h1>')
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5500)
--- extractiveUI_streamlit.py
+++ extractiveUI_streamlit.py
@@ -1,18 +1,27 @@
+import json
 import pandas as pd
 from PIL import Image
+import requests
 import streamlit 
 
+FLASK_SERVER_URL = 'http://127.0.0.1:5500/url' 
+
 rp_logo_img = Image.open('img\\rplogo.png')
-streamlit.image(rp_logo_img, width=500, caption='Tech Immersion Placement Programme - Version 0.01', format='PNG')
+streamlit.image(rp_logo_img, width=500, caption='Tech Immersion Placement Programme - Version 0.01 (Intake 1 2020)', format='PNG')
 
-url_text_input = streamlit.sidebar.text_input('URL:', 'http://')
-url_summarize_btn = streamlit.sidebar.button('Get Summary')
+url_text = streamlit.sidebar.text_input('URL:', 'http://')
 
-streamlit_balloons_btn = streamlit.sidebar.button('Balloons Test')
-if streamlit_balloons_btn:
-    streamlit.balloons()
-
+url_summarize_btn = streamlit.sidebar.button('Summarize')
 if url_summarize_btn:
+
+    # json.loads expect a string as input
+    # it then convert it to a dict
+    payload = json.loads (
+        '{"search_url": ' + '"' + url_text  + '"}'
+    )
+
+    resp = requests.post(FLASK_SERVER_URL, json=payload)
+    streamlit.write('The json is: ', resp.json())
 
     # Display a title
     '''
@@ -33,11 +42,6 @@ if url_summarize_btn:
     for i in range(len(df)):
         agree.append(streamlit.checkbox(df.loc[i][0], value=(i in summarized)))
 
-
-
-
-# # # Select some rows using st.multiselect. This will break down when you have >1000 rows.
-# streamlit.write('### Full Dataset', df)
-# selected_indices = streamlit.multiselect('Selected rows:', df.index.to_list(), df.loc[df.iloc[:, 0].str.contains('<hl>')].index.tolist())
-# selected_rows = df.loc[selected_indices]
-# streamlit.write('### Selected Rows', selected_rows)
+streamlit_balloons_btn = streamlit.sidebar.button('Balloons!')
+if streamlit_balloons_btn:
+    streamlit.balloons()
